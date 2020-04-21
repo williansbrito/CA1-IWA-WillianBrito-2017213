42e137db42fafaf996d6604934f6fa6b31d4196c
williansbrito <willian_silvabrito@hotmail.com>
2020-04-21 12:32:28 +0000

    connecting the server and the database, also  updating the databse  automatically

--- views/database.js
+++ views/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose'); //requere mongodb
-
-mongoose.connect('mongodb+srv://user:1234@cluster0-w11hc.mongodb.net/test?retryWrites=true&w=majority') // connecting to mongodb
+// connecting to mongodb and updating the databse automatically
+mongoose.connect('mongodb+srv://user:1234@cluster0-w11hc.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}) 
     .then(() => console.log('DB is connected'))// show it on console if its works
     .catch(e => console.log(e));// catch error if don't connect
